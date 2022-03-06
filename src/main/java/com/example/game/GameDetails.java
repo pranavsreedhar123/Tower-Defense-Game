eea9b501a3a933daccfd3a4ec74a6636e73baf02
pranavsreedhar123 <pranavsreedhar2002@gmail.com>
2022-03-05 20:11:45 -0500

    Fixed placing towers

--- src/main/java/com/example/game/GameDetails.java
+++ src/main/java/com/example/game/GameDetails.java
@@ -1,10 +1,17 @@
 package com.example.game;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class GameDetails {
     private String name;
     private String level;
+    private String image;
     private int money;
     private int health;
+    private Button[] backgroundButton = new Button[108];
+    private GridPane map;
 
     public GameDetails(int money, int health, String level, String name) {
         this.money = money;
@@ -24,6 +31,9 @@ public class GameDetails {
     public void setName(String name) {
         this.name = name;
     }
+    public void setImage(String image) {this.image = image; }
+    public void setBackgroundButton(Button[] backgroundButton) {this.backgroundButton = backgroundButton;}
+    public void setMap(GridPane map) {this.map = map;}
     public int getMoney() {
         return money;
     }
@@ -33,4 +43,7 @@ public class GameDetails {
     public String getLevel() {
         return level;
     }
+    public String getImage() { return image; }
+    public Button[] getBackgroundButton() { return backgroundButton; }
+    public GridPane getMap() { return  map;}
 }
--- src/main/java/com/example/game/LandscapeController.java
+++ src/main/java/com/example/game/LandscapeController.java
@@ -13,6 +13,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 //import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.util.Arrays;
 
 //import java.io.IOException;
 import java.io.IOException;
@@ -32,7 +33,8 @@ public class LandscapeController {
     @FXML
     private Button shop;
     @FXML
-    private static Button[] backgroundButtonArray = new Button[108];
+    private Button[] backgroundButtonArray;
+    private Button[] temp = new Button[108];
     @FXML
     private GridPane map;
 
@@ -41,16 +43,17 @@ public class LandscapeController {
 
 
 
+
     @FXML
     private void initialize() {
         gameDetails = StoreGame.getGameDetails();
         String level = StoreGame.getGameDetails().getLevel();
+        backgroundButtonArray = StoreGame.getGameDetails().getBackgroundButton();
         if (gameDetails != null) {
             this.moneyText.setText("$" + gameDetails.getMoney());
             //this.moneyText.toFront();
             this.healthText.setText("/ " + gameDetails.getHealth());
             this.dynamicHealthText.setText(" " + gameDetails.getHealth());
-
             this.shop.requestFocus();
             this.shop.setText("Open Shop!");
             try {
@@ -68,71 +71,95 @@ public class LandscapeController {
                 ColumnConstraints columnConstraints = new ColumnConstraints(100);
                 map.getColumnConstraints().add(columnConstraints);
             }
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 12; j++) {
-                    Button backgroundButton = new Button();
-                    backgroundButton.setId(i + "," + j);
-                    backgroundButton.setPrefSize(100, 100);
 
-                    if ((i == 0 ) && (j == 0)) {
-                        ImageView quitImgView = new ImageView();
-                        quitImgView.setImage(quitImg);
-                        quit.setMaxSize(100, 100);
-                        quitImgView.setFitHeight(100);
-                        quitImgView.setFitWidth(92);
-                        quitImgView.setPreserveRatio(true);
-                        quit.setGraphic(quitImgView);
-                        quit.requestFocus();
-                        quitImgView.setImage(quitImg);
-                        quit.setStyle("-fx-background-color: #ff0000");
-                        quit.toFront();
-                        continue;
-                    } else if ((i == 0) && (j == 11)) {
-                       // structure.setAlignment(moneyText, Pos.TOP_RIGHT);
-                        //shop.setStyle("-fx-background-color: #00ff00");
-                        shop.setPrefSize(100, 100);
-                        map.setRowIndex(shop, 0);
-                        map.setColumnIndex(shop, 11);
-                        continue;
-                    } else if ((i == 1) && (j == 11)) {
-                        // structure.setAlignment(moneyText, Pos.TOP_RIGHT);
-                        moneyText.setStyle("-fx-background-color: #00ff00");
-                        map.setRowIndex(moneyText, 1);
-                        map.setColumnIndex(moneyText, 11);
-                        continue;
-                    } else if ((i == 3) && (j == 7)) {
-                        healthText.setStyle("-fx-background-color: #ff0000");
-                        map.setRowIndex(healthText, 3);
-                        map.setColumnIndex(healthText, 7);
-                        continue;
-                    } else if ((i == 1) && (j == 0 || j == 1)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    }  else if ((i == 2 || i == 3) && (j == 1)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    } else if ((i == 3) && (j == 2 || j == 3)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    } else if ((i == 4 || i == 5 || i == 6) && (j == 3)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    } else if ((i == 6 ) && (j == 4 || j == 5 || j == 6 || j == 7 || j == 8 || j == 9 || j == 10)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    } else if ((i == 3 || i == 4 || i == 5) && (j == 10)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    } else if ((i == 3) && (j == 8 || j == 9)) {
-                        backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
-                    } else if ((i == 2 || i == 4) && (j == 7 || j == 6)) {
-                        backgroundButton.setStyle("-fx-background-color: #ff0000; -fx-background-radius: 0");
-                    } else if ((i == 3) && (j == 6)) {
-                        dynamicHealthText.setStyle("-fx-background-color: #ff0000");
-                        map.setRowIndex(dynamicHealthText, 3);
-                        map.setColumnIndex(dynamicHealthText, 6);
-                        continue;
-                    }  else {
-                       backgroundButton.setStyle("-fx-background-color: #00ff00; -fx-background-radius: 0");
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        Button backgroundButton = new Button();
+                        backgroundButton.setId(i + "," + j);
+                        backgroundButton.setPrefSize(100, 100);
 
+                        if ((i == 0) && (j == 0)) {
+                            ImageView quitImgView = new ImageView();
+                            quitImgView.setImage(quitImg);
+                            quit.setMaxSize(100, 100);
+                            quitImgView.setFitHeight(100);
+                            quitImgView.setFitWidth(92);
+                            quitImgView.setPreserveRatio(true);
+                            quit.setGraphic(quitImgView);
+                            quit.requestFocus();
+                            quitImgView.setImage(quitImg);
+                            quit.setStyle("-fx-background-color: #ff0000");
+                            quit.toFront();
+                            continue;
+                        } else if ((i == 0) && (j == 11)) {
+                            // structure.setAlignment(moneyText, Pos.TOP_RIGHT);
+                            //shop.setStyle("-fx-background-color: #00ff00");
+                            shop.setPrefSize(100, 100);
+                            map.setRowIndex(shop, 0);
+                            map.setColumnIndex(shop, 11);
+                            continue;
+                        } else if ((i == 1) && (j == 11)) {
+                            // structure.setAlignment(moneyText, Pos.TOP_RIGHT);
+                            moneyText.setStyle("-fx-background-color: #00ff00");
+                            map.setRowIndex(moneyText, 1);
+                            map.setColumnIndex(moneyText, 11);
+                            continue;
+                        } else if ((i == 3) && (j == 7)) {
+                            healthText.setStyle("-fx-background-color: #ff0000");
+                            map.setRowIndex(healthText, 3);
+                            map.setColumnIndex(healthText, 7);
+                            continue;
+                        } else if ((i == 1) && (j == 0 || j == 1)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 2 || i == 3) && (j == 1)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 3) && (j == 2 || j == 3)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 4 || i == 5 || i == 6) && (j == 3)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 6) && (j == 4 || j == 5 || j == 6 || j == 7 || j == 8 || j == 9 || j == 10)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 3 || i == 4 || i == 5) && (j == 10)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 3) && (j == 8 || j == 9)) {
+                            backgroundButton.setStyle("-fx-background-color: #0000ff; -fx-background-radius: 0");
+                        } else if ((i == 2 || i == 4) && (j == 7 || j == 6)) {
+                            backgroundButton.setStyle("-fx-background-color: #ff0000; -fx-background-radius: 0");
+                        } else if ((i == 3) && (j == 6)) {
+                            dynamicHealthText.setStyle("-fx-background-color: #ff0000");
+                            map.setRowIndex(dynamicHealthText, 3);
+                            map.setColumnIndex(dynamicHealthText, 6);
+                            continue;
+                        } else {
+                            if (backgroundButtonArray[12*i + j] != null && backgroundButtonArray[12*i + j].getGraphic() == null) {
+                                backgroundButton.setStyle("-fx-background-color: #00ff00; -fx-background-radius: 0");
+                            } else if (backgroundButtonArray[12*i + j] == null) {
+                                backgroundButton.setStyle("-fx-background-color: #00ff00; -fx-background-radius: 0");
+                            } else {
+                                backgroundButton.setGraphic(backgroundButtonArray[12*i + j].getGraphic());
+                            }
+                            backgroundButton.setOnAction(e -> {
+                        /*try {
+                            placeTower(button);
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }*/
+                                if (StoreGame.getGameDetails().getImage().equals("normal")) {
+                                    placeTower(backgroundButton, "normal");
+                                } else if (StoreGame.getGameDetails().getImage().equals("elite")) {
+                                    placeTower(backgroundButton, "elite");
+                                } else if (StoreGame.getGameDetails().getImage().equals("bad")) {
+                                    placeTower(backgroundButton, "bad");
+                                }
+
+
+                            });
+                        }
+                        map.add(backgroundButton, j, i);
+                        backgroundButtonArray[12 * i + j] = backgroundButton;
+                        StoreGame.getGameDetails().setBackgroundButton(backgroundButtonArray);
                     }
-                    map.add(backgroundButton, j, i);
-                    backgroundButtonArray[12*i + j] = backgroundButton;
-                }
+
             }
             /*for (Button button: backgroundButtonArray) {
                 if (button != null) {
@@ -154,7 +181,7 @@ public class LandscapeController {
         FXMLLoader configPaneLoader = new FXMLLoader(
                 TowerDefenseApplication.class.getResource("configuration.fxml"));
         Parent configScreen = configPaneLoader.load();
-        Scene configScene = new Scene(configScreen, 1200, 1000);
+        Scene configScene = new Scene(configScreen, 1200, 900);
         configScene.getRoot().setStyle("-fx-font-family: 'Arial'");
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         stage.setTitle("Configuration");
@@ -166,7 +193,7 @@ public class LandscapeController {
         FXMLLoader configPaneLoader = new FXMLLoader(
                 TowerDefenseApplication.class.getResource("shop.fxml"));
         Parent configScreen = configPaneLoader.load();
-        Scene configScene = new Scene(configScreen, 1200, 1000);
+        Scene configScene = new Scene(configScreen, 1200, 900);
         configScene.getRoot().setStyle("-fx-font-family: 'Arial'");
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         stage.setTitle("Shop");
@@ -174,7 +201,7 @@ public class LandscapeController {
     }
 
     protected void clickTower() throws java.io.IOException {
-        for (Button button: backgroundButtonArray) {
+        for (Button button: StoreGame.getGameDetails().getBackgroundButton()) {
             if (button != null) {
                 if (button.getStyle().equals("-fx-background-color: #00ff00; -fx-background-radius: 0")) {
                     button.setOnAction(e -> {
@@ -183,36 +210,73 @@ public class LandscapeController {
                     } catch (IOException ioException) {
                         ioException.printStackTrace();
                     }*/
-                        placeTower(button);
+                        if (StoreGame.getGameDetails().getImage().equals("normal")) {
+                            placeTower(button, "normal");
+                        } else if (StoreGame.getGameDetails().getImage().equals("elite")) {
+                            placeTower(button, "elite");
+                        } else if (StoreGame.getGameDetails().getImage().equals("bad")) {
+                            placeTower(button, "bad");
+                        }
+
 
                     });
                 }
             }
         }
     }
-    protected void placeTower(Button backgroundButton)  {
+    protected void placeTower(Button backgroundButton, String tower)  {
         String[] id = backgroundButton.getId().split(",");
         System.out.println(backgroundButton.getStyle());
         int row = Integer.parseInt(id[0]);
         int col = Integer.parseInt(id[1]);
+        if (tower.equals("bad")) {
+            URL url = TowerDefenseApplication.class.getResource("assets/images/BadTower.png");
+            Image towerImg = new Image(String.valueOf(url));
 
-        URL url = TowerDefenseApplication.class.getResource("assets/images/Tower.png");
-        Image towerImg = new Image(String.valueOf(url));
+            ImageView towerImgView = new ImageView();
+            towerImgView.setImage(towerImg);
+            //quit.setMaxSize(100, 100);
+            towerImgView.setFitHeight(100);
+            towerImgView.setFitWidth(92);
+            towerImgView.setPreserveRatio(true);
+            backgroundButton.setGraphic(towerImgView);
+            backgroundButtonArray[12*row + col] = backgroundButton;
+            StoreGame.getGameDetails().setBackgroundButton(backgroundButtonArray);
+        } else if (tower.equals("normal")) {
+            URL url = TowerDefenseApplication.class.getResource("assets/images/NormalTower.png");
+            Image towerImg = new Image(String.valueOf(url));
 
-        ImageView towerImgView = new ImageView();
-        towerImgView.setImage(towerImg);
-        //quit.setMaxSize(100, 100);
-        towerImgView.setFitHeight(100);
-        towerImgView.setFitWidth(92);
-        towerImgView.setPreserveRatio(true);
-        backgroundButton.setGraphic(towerImgView);
+            ImageView towerImgView = new ImageView();
+            towerImgView.setImage(towerImg);
+            //quit.setMaxSize(100, 100);
+            towerImgView.setFitHeight(100);
+            towerImgView.setFitWidth(92);
+            towerImgView.setPreserveRatio(true);
+            backgroundButton.setGraphic(towerImgView);
+            backgroundButtonArray[12*row + col] = backgroundButton;
+            StoreGame.getGameDetails().setBackgroundButton(backgroundButtonArray);
+        } else if (tower.equals("elite")) {
+            URL url = TowerDefenseApplication.class.getResource("assets/images/EliteTower.png");
+            Image towerImg = new Image(String.valueOf(url));
+
+            ImageView towerImgView = new ImageView();
+            towerImgView.setImage(towerImg);
+            //quit.setMaxSize(100, 100);
+            towerImgView.setFitHeight(100);
+            towerImgView.setFitWidth(92);
+            towerImgView.setPreserveRatio(true);
+            backgroundButton.setGraphic(towerImgView);
+            backgroundButtonArray[12*row + col] = backgroundButton;
+            StoreGame.getGameDetails().setBackgroundButton(backgroundButtonArray);
+        }
         //quit.requestFocus();
-        towerImgView.setImage(towerImg);
+        //towerImgView.setImage(towerImg);
         System.out.println(row);
         System.out.println(col);
-        map.setRowIndex(towerImgView, row);
-        map.setColumnIndex(towerImgView, col);
-        disableTowerButtons();
+        map.setRowIndex(backgroundButton, row);
+        map.setColumnIndex(backgroundButton, col);
+        StoreGame.getGameDetails().setImage("");
+        //disableTowerButtons();
         //backgroundButton.setStyle("-fx-background-color: #ff0000");
 
         //quit.toFront();
@@ -230,11 +294,11 @@ public class LandscapeController {
     in ShopController so that when you go back to the shop
     you can place a tower again.
      */
-    protected void enableTowerButtons() {
-        for (Button b: backgroundButtonArray) {
-            if (b != null) {
-                b.setDisable(false);
-            }
-        }
-    }
+//    protected void enableTowerButtons() {
+//        for (Button b: backgroundButtonArray) {
+//            if (b != null) {
+//                b.setDisable(false);
+//            }
+//        }
+//    }
 }
